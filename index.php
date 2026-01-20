a1a68d1569fdef6550c26bb05ea86fad57bbedc5
Hunter Wilson <hello@hunterwilson.dev>
2026-01-20 10:54:48 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hunter's Home Page</title>
+</head>
+<body>
+
+<h1>Hunter's Home Page</h1>
+<p>Welcome to Hunter's Home page. Here you can see my CIS 3870 projects.</p>
+
+</body>
+</html>
