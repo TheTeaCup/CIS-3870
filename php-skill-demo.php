df174ecf27d1ef20c6e508bfd32cc7d41b3d4719
Hunter Wilson <lw0482352@gmail.com>
2026-01-22 10:48:52 -0500

    Document form validation rules in php-skill-demo.php
    Add PHP form validation requirements as comments

---
+++ php-skill-demo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The form must be submitted to PHP code that verifies:
+ * That all of the fields have been filled in
+ * That BannerID is a 9-digit number
+ * That FirstName is 15 characters or less
+ * That FirstName and LastName do not exceed 30 characters total
+ * That Email is in the proper format (use FILTER_VALIDATE_EMAIL)
+ * That the values in both password fields match, contain at least one number and one letter, and are more than 12 characters long
+ * 
+ * Submitted Values: BannerID, FirstName, LastName, EmailAddr, Pwd, ConfPwd
+ */
+
+
+?>
+
