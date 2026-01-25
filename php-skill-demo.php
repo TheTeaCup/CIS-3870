446b24c3d1ba399b0adeae0a8b4283a960a543b5
TheTeaCup <lw0482352@gmail.com>
2026-01-24 23:40:10 -0500

    working...

--- php-skill-demo.php
+++ php-skill-demo.php
@@ -11,6 +11,70 @@
  * Submitted Values: BannerID, FirstName, LastName, EmailAddr, Pwd, ConfPwd
  */
 
+$ValidForm = true;
+$FormMessage = "";
+$BannerID = $_POST["BannerID"] ?? '';
+$FirstName = $_POST["FirstName"] ?? '';
+$LastName = $_POST["LastName"] ?? '';
+$EmailAddr = $_POST["EmailAddr"] ?? '';
+$Pwd = $_POST["Pwd"] ?? '';
+$ConfPwd = $_POST["ConfPwd"] ?? '';
+
+if ($BannerID) {
+    if (!is_numeric($BannerID) || strlen($BannerID) != 9) {
+        $FormMessage .= "Error: BannerID must be a 9-digit number. <br>";
+        $ValidForm = false;
+    }
+} else {
+    $FormMessage .= "Error: BannerID must have a value. <br>";
+    $ValidForm = false;
+}
+
+if ($FirstName) {
+    if (strlen($FirstName) > 15) {
+        $FormMessage .= "Error: FirstName is longer than 15 characters. <br>";
+        $ValidForm = false;
+    }
+} else {
+    $FormMessage .= "Error: FirstName must have a value. <br>";
+    $ValidForm = false;
+}
+
+if ($LastName) {
+    // no specific validation for LastName
+} else {
+    $FormMessage .= "Error: LastName must have a value. <br>";
+    $ValidForm = false;
+}
+
+if (strlen($FirstName) + strlen($LastName) > 30) {
+    $FormMessage .= "Error: FirstName and LastName exceed 30 characters total. <br>";
+    $ValidForm = false;
+}
+
+if ($EmailAddr) {
+    if (!filter_var($EmailAddr, FILTER_VALIDATE_EMAIL)) {
+        $FormMessage .= "Error: EmailAddr is not in a valid format. <br>";
+        $ValidForm = false;
+    }
+} else {
+    $FormMessage .= "Error: EmailAddr must have a value. <br>";
+    $ValidForm = false;
+}
+
+if ($Pwd && $ConfPwd) {
+    if ($Pwd !== $ConfPwd) {
+        $FormMessage .= "Error: Passwords do not match. <br>";
+        $ValidForm = false;
+    }
+    if (strlen($Pwd) < 12 || !preg_match('/[A-Za-z]/', $Pwd) || !preg_match('/[0-9]/', $Pwd)) {
+        $FormMessage .= "Error: Password must be more than 12 characters long and contain at least one letter and one number. <br>";
+        $ValidForm = false;
+    }
+} else {
+    $FormMessage .= "Error: Both password fields must have values. <br>";
+    $ValidForm = false;
+}
 
 ?>
 
