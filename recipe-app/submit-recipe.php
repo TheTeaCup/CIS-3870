cc709753cd891cac4ce2abb6acd1ae8dab47c42a
TheTeaCup <lw0482352@gmail.com>
2026-02-02 23:24:42 -0500

    updated

--- recipe-app/submit-recipe.php
+++ recipe-app/submit-recipe.php
@@ -1,6 +1,14 @@
 <?php
 // we need a variable to keep track if any values are missing.
 $ValidForm = true;
+$RecipeID = htmlspecialchars($_POST["RecipeID"] ?? "");
+$RecipeTitle = htmlspecialchars($_POST["RecipeTitle"] ?? "");
+$RecipeDesc = htmlspecialchars($_POST["RecipeDesc"] ?? "");
+$MakesQty = htmlspecialchars($_POST["MakesQty"] ?? "");
+$MakesType = htmlspecialchars($_POST["MakesType"] ?? "");
+$PrepMins = htmlspecialchars($_POST["PrepMins"] ?? "");
+$Category = htmlspecialchars($_POST["Category"] ?? "");
+$Picture = htmlspecialchars($_POST["Picture"] ?? "");
 
 // we want to check if the user entered values in required fields.
 if (!$_POST["RecipeID"]) {
@@ -19,10 +27,32 @@ if (!$_POST["RecipeTitle"]) {
     echo "Error: RecipeTitle must have a value. <br>";
     $ValidForm = false;
 } else {
-    if(strlen($_POST["RecipeTitle"]) > 75) {
+    if (strlen($_POST["RecipeTitle"]) > 75) {
         echo "RecipeTitle is longer than 75 characters. <br>";
         $ValidForm = false;
     }
+}
+
+if (!preg_match('/[A-Z]/', $Category)) {
+    //after the else, this executes when they DIDN'T use an upper case letter
+    echo "Category must have at least one uppercase letter.<br>";
+    $ValidForm = false;
+}
+
+if (!preg_match('/[a-z]/', $Category)) {
+    echo "Category must have at least one lowercase letter.<br>";
+    $ValidForm = false;
+}
+
+if (!preg_match('/[0-9]/', $Category)) {
+    echo "Category must have at least one number.<br>";
+    $ValidForm = false;
+}
+
+// Validate that picture is a URL (Later, we will change this to a local file)
+if (!filter_var($Picture, FILTER_VALIDATE_URL) == true) {
+    echo $Picture . " is not a valid URL.<br>"; //the dot character is used to append text
+    $ValidForm = false;
 }
 
 // after checking all required value
@@ -33,29 +63,46 @@ if (!$ValidForm) {
 }
 ?>
 
+<!DOCTYPE html>
 <html>
+
 <body>
 
-<h2>Recipe Submitted</h2>
+    <h2>Recipe Submitted</h2>
 
-<!-- 
-!!! the code form w3schools enabled cross site scripting which is a valnerbility...
-The ?php tag starts the section of php code and end the section with ? 
-echo command tells the server to print/send that text to the client (as html or whatever the form of the file is)
-$_POST is an array that contains all the submitted data from the client using the POST command
-["RecipeID"] references that part of the array and gets the data 
--->
-<p>
-    <strong>RecipeID:</strong> <?php echo htmlspecialchars($_POST["RecipeID"] ?? ""); ?><br><br>
-    <strong>RecipeTitle:</strong> <?php echo htmlspecialchars($_POST["RecipeTitle"] ?? ""); ?><br><br>
-    <strong>RecipeDesc:</strong> <?php echo htmlspecialchars($_POST["RecipeDesc"] ?? ""); ?><br><br>
-    <strong>MakesQty:</strong> <?php echo htmlspecialchars($_POST["MakesQty"] ?? ""); ?><br><br>
-    <strong>MakesType:</strong> <?php echo htmlspecialchars($_POST["MakesType"] ?? ""); ?><br><br>
-    <strong>PrepMins:</strong> <?php echo htmlspecialchars($_POST["PrepMins"] ?? ""); ?><br><br>
-    <strong>Category:</strong> <?php echo htmlspecialchars($_POST["Category"] ?? ""); ?><br><br>
-    <strong>Picture:</strong> <?php echo htmlspecialchars($_POST["Picture"] ?? ""); ?><br><br>
-</p>
+    <p><strong>RecipeID:</strong>
+        <?php echo htmlspecialchars($RecipeID, ENT_QUOTES, 'UTF-8'); ?>
+    </p>
 
+    <p><strong>Title:</strong>
+        <?php echo htmlspecialchars($RecipeTitle, ENT_QUOTES, 'UTF-8'); ?>
+    </p>
+
+    <p><strong>Description:</strong>
+        <?php echo htmlspecialchars($RecipeDesc, ENT_QUOTES, 'UTF-8'); ?>
+    </p>
+
+    <p><strong>Makes:</strong>
+        <?php
+        echo htmlspecialchars($MakesQty, ENT_QUOTES, 'UTF-8') . " " .
+            htmlspecialchars($MakesType, ENT_QUOTES, 'UTF-8');
+        ?>
+    </p>
+
+    <p><strong>Prep Minutes:</strong>
+        <?php echo htmlspecialchars($PrepMins, ENT_QUOTES, 'UTF-8'); ?>
+    </p>
+
+    <p><strong>Category:</strong>
+        <?php echo htmlspecialchars($Category, ENT_QUOTES, 'UTF-8'); ?>
+    </p>
+
+    <p><strong>Picture:</strong>
+        <img src="<?php
+        echo htmlspecialchars($Picture, ENT_QUOTES, 'UTF-8');
+        ?>" width="100">
+    </p>
 
 </body>
+
 </html>
