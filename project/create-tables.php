f971a71ba29fcc5c459b0cc7abd898dfd103e763
Hunter Wilson <hello@hunterwilson.dev>
2026-03-30 09:51:48 -0400

    Add files via upload

--- project/create-tables.php
+++ project/create-tables.php
@@ -50,4 +50,16 @@ BidderID    int(PK)
 LotID       int(PK)
 BidTime     datetime(PK)
 BidAmount   decimal(10,2)
+
+CREATE TABLE Donor (DonorID INT NOT NULL, BusinessName VARCHAR(75), ContactName VARCHAR(75), ContactEmail VARCHAR(200), ContactTitle VARCHAR(75), Address VARCHAR(75), City VARCHAR(30), State VARCHAR(2), ZipCode VARCHAR(5), TaxReceipt BOOLEAN, PRIMARY KEY (DonorID));
+
+CREATE TABLE Bidder (BidderID INT NOT NULL, Name VARCHAR(75), Address VARCHAR(75), CellNumber VARCHAR(10), HomeNumber VARCHAR(10), Email VARCHAR(200), Paid BOOLEAN, PRIMARY KEY (BidderID));
+
+CREATE TABLE Category (CategoryID INT NOT NULL, Description VARCHAR(75), PRIMARY KEY (CategoryID));
+
+CREATE TABLE Item (ItemID INT NOT NULL, Description VARCHAR(75), RetailValue DECIMAL(10,2), DonorID INT, LotID INT, PRIMARY KEY (ItemID));
+
+CREATE TABLE Lot (LotID INT NOT NULL, Description VARCHAR(125), CategoryID INT, WinningBid DECIMAL(10,2), WinningBidder INT, Delivered BOOLEAN, PRIMARY KEY (LotID));
+
+CREATE TABLE Bid (LotID INT NOT NULL, BidderID INT NOT NULL, BidTime DATETIME, Bid DECIMAL(10,2), PRIMARY KEY (LotID, BidderID, BidTime));
 -->
