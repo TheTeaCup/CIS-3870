105a2797fc7182c04e58c00109948bbcce48e21d
Hunter Wilson <hello@hunterwilson.dev>
2026-03-31 09:51:54 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
     <h1>Hunter's Home Page</h1>
     <p>Welcome to Hunter's Home page. Here you can see my CIS 3870 projects.</p>
 
+    <p><b>Recipe Application:</b> <a href='/recipe-app'>Click Here</a></p>
+    <p><b>Skill Demonstration:</b> <a href='/php-mysql-skills'>Click Here</a></p>
+    <p><b>Final Project (Group Work):</b> <a href='/project'>Click Here</a></p>
+
 </body>
 
 </html>
