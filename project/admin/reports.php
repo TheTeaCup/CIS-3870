6e1cf9089f17f526e68e5a4111161bfd7b632100
zachhawkins0-bit <zachhawkins0@gmail.com>
2026-05-06 14:20:06 -0400

    Add reports.php for report navigation

---
+++ project/admin/reports.php
@@ -0,0 +1,17 @@
+<?php
+include 'page-header.php';
+echo PageHeader("Reports");
+?>
+
+<div class="center button-container">
+
+    <h1>Reports</h1>
+
+    <a class="button" href="/project/admin/undeliveredlotsreport.php">Undelivered Lots Report</a>
+
+    <a class="button" href="/project/admin/bidderpaymentreport.php">Bidder Payment Report</a>
+
+</div>
+
+</body>
+</html>
